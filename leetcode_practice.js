69f8f43124f14a32343a0379f47b1f1a4776e585
Nero <nerochen25@berkeley.edu>
2019-02-03 17:51:48 -0800

    Add 141. Linked List Cycle

--- leetcode_practice.js
+++ leetcode_practice.js
@@ -339,3 +339,27 @@ var reverse = function(x) {
         return reversedInt;
     }
 };
+
+//141. Linked List Cycle
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+// /**
+//  * @param {ListNode} head
+//  * @return {boolean}
+//  */
+var hasCycle = function(head) {
+    while(head) {
+        if(head.visited) {
+            return true;
+        }
+        head.visited = true;
+        head = head.next;
+    }
+    return false;
+};
