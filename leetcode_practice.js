f84c6cfb1c323d0d312992504bf95f832d212889
Nero <nerochen25@berkeley.edu>
2019-02-03 16:02:37 -0800

    Add 175. Combine Two Tables

--- leetcode_practice.js
+++ leetcode_practice.js
@@ -290,3 +290,8 @@ var maxProfit = function(prices) {
 };
 
 
+//175. Combine Two Tables
+// # Write your MySQL query statement below
+select FirstName, LastName, City, State
+from Person left join Address
+on Person.PersonId = Address.PersonId;
