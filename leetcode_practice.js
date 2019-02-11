7385da27b57ad829c6f9f0590fe16aaab2312e95
Nero <nerochen25@berkeley.edu>
2019-02-10 21:58:13 -0800

    Add permutation coding exercise

--- leetcode_practice.js
+++ leetcode_practice.js
@@ -402,3 +402,17 @@ var licenseKeyFormatting = function(S, K) {
         return groupArr.join('-');
     }
 };
+
+// 3.Algorithm
+// Given a collection of distinct integers, return all possible permutations.
+// Example:
+// Input: [1, 2, 3]
+// Output:
+// [
+// [1, 2, 3],
+// [1, 3, 2],
+// [2, 1, 3],
+// [2, 3, 1],
+// [3, 1, 2],
+// [3, 2, 1],
+// ]
