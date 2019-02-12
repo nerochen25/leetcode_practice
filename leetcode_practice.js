70273134cd042610c041d1d569ab0dcdce98eb30
Nero <nerochen25@berkeley.edu>
2019-02-11 23:17:59 -0800

    Add coding exercise question Trie data type with insert, search and startsWith methods

--- leetcode_practice.js
+++ leetcode_practice.js
@@ -436,3 +436,17 @@ function permutations(array) {
 
   return permute(array);
 }
+
+// 4. Algorithm 2
+// Implement a trie with insert, search, and startsWith methods.
+// Example:
+// Trie trie = new Trie();
+// trie.insert("apple");
+// trie.search("apple"); // returns true
+// trie.search("app"); // returns false
+// trie.startsWith("app"); // returns true
+// trie.insert("app");
+// trie.search("app"); // returns true
+// Note:
+// Ŏ You may assume that all inputs are consist of lowercase letters a-z.
+// Ŏ All inputs are guaranteed to be non-empty strings.
