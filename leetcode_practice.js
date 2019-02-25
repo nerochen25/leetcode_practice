55adb2a7c8ed07545f8eb1945cbc4d1b69912e45
Nero <nerochen25@berkeley.edu>
2019-02-24 22:00:27 -0800

    Add sockMerchant problem

--- leetcode_practice.js
+++ leetcode_practice.js
@@ -587,3 +587,11 @@ var romanToInt = function(s) {
     }
     return result;
 };
+
+
+// Complete the sockMerchant function in the editor below. It must return an integer representing the number of matching pairs of socks that are available.
+
+// sockMerchant has the following parameter(s):
+
+// n: the number of socks in the pile
+// ar: the colors of each sock
